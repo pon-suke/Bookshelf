e56694dcaf7d6061ddf121c950681ba50bf76cfa
YosukeOba <52928718+YosukeOba@users.noreply.github.com>
2022-10-28 22:31:39 +0900

    drawerを追加．スライダーで，books_row_numを変更できるように

--- bookshelf/lib/View/bookshelf_view.dart
+++ bookshelf/lib/View/bookshelf_view.dart
@@ -2,26 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:bookshelf/View/input_view.dart';
 import 'package:bookshelf/View/details_view.dart';
 
-class BookshelfView extends StatelessWidget {
+class BookshelfView extends StatefulWidget {
   const BookshelfView({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    var margin = 10;
-    var books_num = 20;
-    var books_row_num = 3;
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
 
+class _State extends State<BookshelfView> {
+  var margin = 20;
+  var books_num = 20;
+  var books_row_num = 3;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("本棚"),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const InputView()));
-                },
-                icon: const Icon(Icons.add)),
-          ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const InputView()));
+          },
+          child: const Icon(Icons.add),
+        ),
+        endDrawer: Drawer(
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white, border: Border.all(width: 1)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                      child: DrawerHeader(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                          padding: EdgeInsets.zero,
+                          child: Text(
+                            "設定",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                    Container(
+                        margin: EdgeInsets.all(margin.toDouble()),
+                        decoration: BoxDecoration(
+                            color: Colors.white, border: Border.all(width: 1)),
+                        child: Column(
+                          children: [
+                            const Text("本を横に並べる数"),
+                            Slider(
+                                value: books_row_num.toDouble(),
+                                min: 1,
+                                max: 10,
+                                divisions: 10,
+                                onChanged: (double value) {
+                                  setState(() {
+                                    books_row_num =
+                                        value.roundToDouble().toInt();
+                                  });
+                                })
+                          ],
+                        ))
+                  ],
+                ))),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Container(
@@ -78,20 +124,45 @@ class BookshelfView extends StatelessWidget {
             Image.asset(
               'images/cover.jpg',
             ),
-            Text(label),
+            Text(
+              label,
+              maxLines: 1,
+            ),
           ],
         ),
       ),
     );
   }
-}
 
-Widget _noBooks(BuildContext context, int margin, int books_row_num) {
-  var size = MediaQuery.of(context).size;
+  Widget _noBooks(BuildContext context, int margin, int books_row_num) {
+    var size = MediaQuery.of(context).size;
 
-  return Container(
-    width: (size.width - margin) * (1 / (books_row_num + 1)),
-    decoration:
-        BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
-  );
+    return Container(
+      width: (size.width - margin) * (1 / (books_row_num + 1)),
+      decoration:
+          BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
+    );
+  }
+
+  // Widget _drawer(int num) {
+  //   return Container(
+  //       margin: EdgeInsets.all(20),
+  //       decoration:
+  //           BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
+  //       child: Column(
+  //         children: [
+  //           Text("本を横に並べる数"),
+  //           Slider(
+  //               value: .toDouble(),
+  //               min: 1,
+  //               max: 10,
+  //               divisions: 10,
+  //               onChanged: (double value) {
+  //                 setState(() {
+  //                   num = value.roundToDouble().toInt();
+  //                 });
+  //               })
+  //         ],
+  //       ));
+  // }
 }
--- bookshelf/lib/View/details_view.dart
+++ bookshelf/lib/View/details_view.dart
@@ -7,14 +7,36 @@ class DetailsView extends StatelessWidget {
     var margin = 20;
 
     return Scaffold(
-      appBar: AppBar(title: const Text("詳細")),
-      body: Container(
-        margin: EdgeInsets.all(margin.toDouble()),
+        appBar: AppBar(title: const Text("詳細")),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(margin.toDouble()),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white, border: Border.all(width: 1)),
+            child: Column(children: <Widget>[
+              _information("タイトル", "ハンターハンター"),
+              _information("著者", "冨樫義博"),
+            ]),
+          ),
+        ));
+  }
+
+  Widget _information(String title, String book_title) {
+    return Container(
         width: double.infinity,
-        height: double.infinity,
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 7),
         decoration:
             BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
-      ),
-    );
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(title),
+            Text(
+              book_title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ));
   }
 }
