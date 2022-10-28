95ce393b03492065877af5a0f1f606ee8c525450
YosukeOba <52928718+YosukeOba@users.noreply.github.com>
2022-10-28 17:49:26 +0900

    bookshelf_viewのbodyをSingleChildScrollViewに変更
    冊数が多い時にスクロールできるよ

--- bookshelf/lib/View/bookshelf_view.dart
+++ bookshelf/lib/View/bookshelf_view.dart
@@ -6,56 +6,58 @@ class BookshelfView extends StatelessWidget {
   const BookshelfView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var margin = 20;
-    var books_num = 4;
+    var margin = 10;
+    var books_num = 20;
     var books_row_num = 3;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("本棚"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const InputView()));
-              },
-              icon: const Icon(Icons.add)),
-        ],
-      ),
-      body: Container(
-        margin: EdgeInsets.all(margin.toDouble()),
-        width: double.infinity,
-        height: double.infinity,
-        decoration:
-            BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            for (int i = 0; i < books_num / books_row_num; i++)
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 7),
-                decoration: BoxDecoration(
-                    color: Colors.white, border: Border.all(width: 1)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    for (int j = 0; j < books_row_num; j++) ...{
-                      if (i * books_row_num + j + 1 <= books_num) ...{
-                        _books("本のタイトル", context, books_row_num),
-                      } else ...{
-                        _noBooks(context, books_row_num),
-                      }
-                    }
-                  ],
-                ),
-              ),
+        appBar: AppBar(
+          title: const Text("本棚"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const InputView()));
+                },
+                icon: const Icon(Icons.add)),
           ],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            margin: EdgeInsets.all(margin.toDouble()),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white, border: Border.all(width: 1)),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                for (int i = 0; i < books_num / books_row_num; i++)
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 7),
+                    decoration: BoxDecoration(
+                        color: Colors.white, border: Border.all(width: 1)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        for (int j = 0; j < books_row_num; j++) ...{
+                          if (i * books_row_num + j + 1 <= books_num) ...{
+                            _books("本のタイトル", context, margin, books_row_num),
+                          } else ...{
+                            _noBooks(context, margin, books_row_num),
+                          }
+                        }
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ));
   }
 
-  Widget _books(String label, BuildContext context, int books_row_num) {
+  Widget _books(
+      String label, BuildContext context, int margin, int books_row_num) {
     // return Container(
     //   width: 100,
     //   child: Image.asset('images/cover.jpg'),
@@ -68,16 +70,13 @@ class BookshelfView extends StatelessWidget {
             .push(MaterialPageRoute(builder: (context) => const DetailsView()));
       },
       child: Container(
-        width: size.width * (1 / (books_row_num + 1)),
+        width: (size.width - margin) * (1 / (books_row_num + 1)),
         decoration:
             BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
         child: Column(
           children: [
-            FittedBox(
-              fit: BoxFit.contain,
-              child: Image.asset(
-                'images/cover.jpg',
-              ),
+            Image.asset(
+              'images/cover.jpg',
             ),
             Text(label),
           ],
@@ -87,11 +86,11 @@ class BookshelfView extends StatelessWidget {
   }
 }
 
-Widget _noBooks(BuildContext context, int books_row_num) {
+Widget _noBooks(BuildContext context, int margin, int books_row_num) {
   var size = MediaQuery.of(context).size;
 
   return Container(
-    width: size.width * (1 / (books_row_num + 1)),
+    width: (size.width - margin) * (1 / (books_row_num + 1)),
     decoration:
         BoxDecoration(color: Colors.white, border: Border.all(width: 1)),
   );
